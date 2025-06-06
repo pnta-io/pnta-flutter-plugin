d33f8ad9f9fd9dbbf564c761ef8211b6cbd7cc20
martins <me@martinslaucis.lv>
2025-06-06 15:31:30 +0200

    Add error handling for event stream casting

--- lib/pnta_flutter_method_channel.dart
+++ lib/pnta_flutter_method_channel.dart
@@ -53,7 +53,19 @@ class MethodChannelPntaFlutter extends PntaFlutterPlatform {
   Stream<Map<String, dynamic>> get foregroundNotifications {
     _foregroundNotificationsStream ??= _foregroundNotificationsEventChannel
         .receiveBroadcastStream()
-        .map<Map<String, dynamic>>((event) => Map<String, dynamic>.from(event));
+        .map<Map<String, dynamic>>((event) {
+          try {
+            if (event is Map) {
+              return Map<String, dynamic>.from(event);
+            } else {
+              debugPrint('Invalid foreground notification event type: ${event.runtimeType}');
+              return <String, dynamic>{};
+            }
+          } catch (e) {
+            debugPrint('Error parsing foreground notification: $e');
+            return <String, dynamic>{};
+          }
+        });
     return _foregroundNotificationsStream!;
   }
 
@@ -69,7 +81,19 @@ class MethodChannelPntaFlutter extends PntaFlutterPlatform {
   Stream<Map<String, dynamic>> get onNotificationTap {
     _notificationTapStream ??= _notificationTapEventChannel
         .receiveBroadcastStream()
-        .map<Map<String, dynamic>>((event) => Map<String, dynamic>.from(event));
+        .map<Map<String, dynamic>>((event) {
+          try {
+            if (event is Map) {
+              return Map<String, dynamic>.from(event);
+            } else {
+              debugPrint('Invalid notification tap event type: ${event.runtimeType}');
+              return <String, dynamic>{};
+            }
+          } catch (e) {
+            debugPrint('Error parsing notification tap: $e');
+            return <String, dynamic>{};
+          }
+        });
     return _notificationTapStream!;
   }
 }
