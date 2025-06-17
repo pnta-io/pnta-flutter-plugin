a0c4eb0d6c5b6a1780eb36884c394bc272334f0c
martins <me@martinslaucis.lv>
2025-06-17 11:51:57 +0200

    Improve iOS permission checking to support provisional and ephemeral states
    - Add support for .provisional and .ephemeral notification statuses
    - Ensure main thread dispatch consistency for pre-iOS 10 branch
    - All notification-capable permission states now properly detected

--- ios/Classes/PermissionHandler.swift
+++ ios/Classes/PermissionHandler.swift
@@ -20,13 +20,17 @@ class PermissionHandler {
         if #available(iOS 10.0, *) {
             UNUserNotificationCenter.current().getNotificationSettings { settings in
                 DispatchQueue.main.async {
-                    let granted = settings.authorizationStatus == .authorized
+                    let granted = settings.authorizationStatus == .authorized ||
+                                  settings.authorizationStatus == .provisional ||
+                                  settings.authorizationStatus == .ephemeral
                     result(granted)
                 }
             }
         } else {
             // For iOS versions below 10, permissions are granted at app install time
-            result(true)
+            DispatchQueue.main.async {
+                result(true)
+            }
         }
     }
 } 
